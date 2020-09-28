486cbefa25dfd1982d1bb3d68ed31fad926fab3a
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-09-28 13:28:14 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+// name: magnus
+// date: 9/28/2020
+// code: hungryness counter is at 0
+let hungryness = 0
+// 
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        hungryness = hungryness + 1
+        basic.showNumber(hungryness)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        hungryness = 0
+        basic.showNumber(hungryness)
+    }
+})
